01f132b9e230e5cd6bfefbfb13b6210080b7c10d
Clicia P Freitas <89753006+Cliciaf@users.noreply.github.com>
2024-08-24 01:29:26 +0000

    atividade arvore

---
+++ Arvores/exercicio.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct NoArvore 
+{
+    int dado;
+    struct NoArvore *esq;
+    struct NoArvore *dir;
+
+};
+
+struct NoArvore *criarNo(int dado)
+{
+
+     struct NoArvore *novoNo = (struct NoArvore *)malloc(sizeof(struct NoArvore));
+    if (novoNo == NULL)
+    {
+        printf("Erro: Falha ao alocar memória para o novo nó.\n");
+        exit(-1);
+    }
+    novoNo->dado = dado;
+    novoNo->esq = NULL;
+    novoNo->dir = NULL;
+    return novoNo;
+}
+
+//inspirado no professor gustavo
+struct NoArvore *inserirNo( struct NoArvore *raiz ,int dado)
+{
+
+    if(raiz == NULL)
+    {
+
+        raiz = criarNo(dado);
+
+    }
+    else
+    {
+        if(dado <= raiz->dado)
+        {
+            raiz->esq = inserirNo(raiz->esq, dado);
+        }
+        else
+        {
+            raiz->dir = inserirNo(raiz->dir, dado);
+        }
+        
+        return raiz;
+    }
+
+
+};
+
+struct NoArvore *excluirNo( struct NoArvore *raiz ,int dado)
+{
+
+
+    if( raiz->dado == dado)
+    {
+        raiz->
+    };
+
+};
+
+// Função para exibir a árvore no formato esquerda-raiz-direita segundo Sedgewick
+void mostraArvore(struct NoArvore *a, int b)
+{
+    if (a == NULL)
+    {
+        return;
+    }
+    mostraArvore(a->dir, b + 1);
+    imprimeNo(a->dado, b); // Convertendo para caractere para imprimir
+    mostraArvore(a->esq, b + 1);
+}
+
+
+
+int main()
+{
+    struct NoArvore *raiz = NULL;
+
+    // Inserindo elementos na árvore
+    raiz = inserir(raiz, 1);
+    raiz = inserir(raiz, 2);
+    raiz = inserir(raiz, 4);
+    raiz = inserir(raiz, 3);
+
+     mostraArvore(raiz, 3);
+
+};
+
